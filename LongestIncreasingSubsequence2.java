ffd10482409bf57f1df0a52039d810c4f3c1b8ff
Success2014 <neogoyeah@gmail.com>
2015-11-13 21:47:43 -0600

    v1

---
+++ LongestIncreasingSubsequence2.java
@@ -0,0 +1,27 @@
+package first;
+
+public class LongestIncreasingSubsequence2 {
+	public int longestIncreasingSubsequence(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int max = 1;
+        for (int i = 1; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] <= nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
+}
