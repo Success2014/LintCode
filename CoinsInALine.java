8381815f95b9a626b8e362c77da589b5dec8054e
Success2014 <neogoyeah@gmail.com>
2015-12-01 22:56:48 -0600

    v1

---
+++ CoinsInALine.java
@@ -0,0 +1,11 @@
+package first;
+
+public class CoinsInALine {
+	public boolean firstWillWin(int n) {
+        if (n % 3 == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
