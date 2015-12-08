a67190b6b40bce953956feefbe4f47a49a74102c
Success2014 <neogoyeah@gmail.com>
2015-12-07 18:55:58 -0600

    v1

---
+++ Backpack.java
@@ -0,0 +1,33 @@
+package first;
+
+public class Backpack {
+	public int backPack(int m, int[] A) {
+        int n = A.length;
+        if (n == 0) {
+            return 0;
+        }
+        boolean[][] dp = new boolean[n + 1][m + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (dp[i - 1][j]) {
+                    dp[i][j] = true;
+                }
+                if (j - A[i - 1] >= 0 && dp[i - 1][j - A[i - 1]]) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+        
+        for (int i = m; i >= 1; i--) {
+            if (dp[n][i]) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+}
