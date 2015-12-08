48e4578fd9060300ef3b2ba8b18d1375be1b3162
Success2014 <neogoyeah@gmail.com>
2015-12-07 19:06:46 -0600

    v1

--- Backpack.java
+++ Backpack.java
@@ -1,6 +1,12 @@
 package first;
 
 public class Backpack {
+	/**
+	 * O(nm) time, O(nm) space.
+	 * @param m
+	 * @param A
+	 * @return
+	 */
 	public int backPack(int m, int[] A) {
         int n = A.length;
         if (n == 0) {
@@ -24,6 +30,40 @@ public class Backpack {
         
         for (int i = m; i >= 1; i--) {
             if (dp[n][i]) {
+                return i;
+            }
+        }
+        return 0;
+    }
+	
+	/**
+	 * O(nm) time, O(m) space.
+	 * @param m
+	 * @param A
+	 * @return
+	 */
+	public int backPack2(int m, int[] A) {
+        int n = A.length;
+        if (n == 0) {
+            return 0;
+        }
+        
+        boolean[] dp = new boolean[m + 1];
+        dp[0] = true;
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = m; j >= 1; j--) {
+                if (dp[j]) {
+                    continue;
+                }
+                if (j - A[i - 1] >= 0 && dp[j - A[i - 1]]) {
+                    dp[j] = true;
+                }
+            }
+        }
+        
+        for (int i = m; i >= 1; i--) {
+            if (dp[i]) {
                 return i;
             }
         }
